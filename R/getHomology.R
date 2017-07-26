26d3800a793c1709ba80b834276a60f05ce15d48
wfondrie <wfondrie@umaryland.edu>
2017-07-26 10:45:30 -0400

    now retrieves homology info. need to plot

--- R/getHomology.R
+++ R/getHomology.R
@@ -3,24 +3,66 @@ library(httr)
 library(stringr)
 library(forcats)
 library(biomaRt)
+library(Biostrings)
 
-# Set Marts
 human <- useEnsembl("ensembl", dataset = "hsapiens_gene_ensembl")
-ptroglodytes <- useEnsembl("ensembl", dataset = "ptroglodytes_gene_ensembl")
-mmulatta <- useEnsembl("ensembl", dataset = "mmulatta_gene_ensembl")
-clupus <- useEnsembl("ensembl", dataset = "mmulatta_gene_ensembl")
-btaurus <- useEnsembl("ensembl", dataset = "btaurus_gene_ensembl")
-mmusculus <- useEnsembl("ensembl", dataset = "mmusculus_gene_ensembl")
-rnorvegicus <- useEnsembl("ensembl", dataset = "rnorvegicus_gene_ensembl")
-ggallus <- useEnsembl("ensembl", dataset = "ggallus_gene_ensembl")
-xtropicalis <- useEnsembl("ensembl", dataset = "xtropicalis_gene_ensembl")
-drerio <- useEnsembl("ensembl", dataset = "drerio_gene_ensembl")
-dmelanogaster <- useEnsembl("ensembl", dataset = "dmelanogaster_gene_ensembl")
-celegans <- useEnsembl("ensembl", dataset = "celegans_gene_ensembl")
 
+lrpFam <- tibble(geneName = as.factor(c("LDLR", "VLDLR", "LRP8", "LRP4", "LRP1", "LRP1B", "LRP2",
+                                        "LRPAP1", "A2M", "APOE", "F8", "PDGFB", "TGFB1")),
+                 altGeneName = as.factor(c("LDLR", "VLDLR", "APOER2", "LRP4", "LRP1", "LRP1B", "Megalin",
+                                           "RAP", "A2M", "APOE", "FVIII", "PDGFB", "TGFB1")),
+                 ensembl_gene_id = c("ENSG00000130164",
+                                     "ENSG00000147852",
+                                     "ENSG00000157193",
+                                     "ENSG00000134569",
+                                     "ENSG00000123384",
+                                     "ENSG00000168702",
+                                     "ENSG00000081479",
+                                     "ENSG00000163956",
+                                     "ENSG00000175899",
+                                     "ENSG00000130203",
+                                     "ENSG00000185010",
+                                     "ENSG00000100311",
+                                     "ENSG00000105329"))
 
-alignmentData <- GET("https://www.ncbi.nlm.nih.gov/gene/?Term=ortholog_gene_4292[group]")
+attrEndings <- c("_homolog_ensembl_gene",
+                 "_homolog_orthology_type",
+                 "_homolog_associated_gene_name",
+                 "_homolog_perc_id")
 
-searchData <- GET("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&Term=ortholog_gene_4292[group]")
+species <- c("ptroglodytes",
+             "mmulatta",
+             "cfamiliaris",
+             "btaurus",
+             "mmusculus",
+             "rnorvegicus",
+             "ggallus",
+             "xtropicalis",
+             "drerio",
+             "dmelanogaster",
+             "celegans")
 
-dat <- content(searchData)
+queryAttr1 <- unlist(map(species[1:6], paste0, attrEndings))
+queryAttr2 <- unlist(map(species[7:11], paste0, attrEndings))
+
+queryAttr1 <- c("ensembl_gene_id", "description", "external_gene_name", queryAttr1)
+queryAttr2 <- c("ensembl_gene_id", "description", "external_gene_name", queryAttr2)
+
+query1 <- getBM(attributes = queryAttr1,
+                filters = "ensembl_gene_id",
+                values = lrpFam$ensembl_gene_id,
+                mart = human)
+
+query2 <- getBM(attributes = queryAttr2,
+                filters = "ensembl_gene_id",
+                values = lrpFam$ensembl_gene_id,
+                mart = human)
+
+results <- query1 %>%
+    full_join(query2, by = c("ensembl_gene_id", "description", "external_gene_name")) %>%
+    gather(attr, value, -ensembl_gene_id, -description, -external_gene_name) %>%
+    group_by(ensembl_gene_id) %>%
+    mutate(species = str_match(attr, "(^.*?)_")[ , 2],
+           category = str_match(attr, "_(.*$)")[ , 2]) %>%
+    dplyr::select(-attr) %>%
+    spread(category, value)
